ca970f73217743648f633ddd7a9b8f4fa255518d
Zohaib Baig (ZOHAIB) <zohaibbaig144@gmail.com>
2025-10-16 22:32:04 +0000

    Playback of song will not pause if you go to settings option

--- src/components/app-shell.tsx
+++ src/components/app-shell.tsx
@@ -49,7 +49,7 @@ export function AppShell({ children }: { children: React.ReactNode }) {
     return <>{children}</>;
   }
 
-  const showBottomNav = !['/profile', '/settings', '/whats-new'].some(path => isActive(path));
+  const showBottomNav = true;
 
 
   return (
